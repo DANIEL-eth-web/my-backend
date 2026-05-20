27ccd646ecba0e1e5d441fac90a80533abf02078
Daniel <Safepalapp.io@gmail.com>
2026-05-20 16:31:35 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,17 +3,32 @@ const app = express();
 
 app.use(express.json());
 
-let balance = 0;
+// simple in-memory database
+let users = {};
 
 // deposit money
 app.post("/deposit", (req, res) => {
-  balance += req.body.amount;
-  res.json({ message: "Deposit successful", balance });
+  const { userId, amount } = req.body;
+
+  if (!users[userId]) {
+    users[userId] = { balance: 0 };
+  }
+
+  users[userId].balance += Number(amount);
+
+  res.json({
+    message: "Deposit successful",
+    balance: users[userId].balance
+  });
 });
 
 // check balance
-app.get("/balance", (req, res) => {
-  res.json({ balance });
+app.get("/balance/:userId", (req, res) => {
+  const user = users[req.params.userId];
+
+  res.json({
+    balance: user ? user.balance : 0
+  });
 });
 
 app.listen(3000, () => console.log("Server running"));
